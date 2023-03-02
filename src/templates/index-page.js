561489380fdf6706a840d26f178d1d266abaf448
Jammer0n <ijsamuel@gmail.com>
2023-03-02 16:44:57 -0500

    Removed from one more spot

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -39,9 +39,6 @@ export const IndexPageTemplate = ({
                   </div>
                   <div className="columns">
                     <div className="column is-12">
-                      <h3 className="has-text-weight-semibold is-size-2">
-                        {heading}
-                      </h3>
                       <p>{description}</p>
                     </div>
                   </div>
