80f6d72b14e781b3cd00200c5537035805aef9e1
Jammer0n <ijsamuel@gmail.com>
2023-03-02 17:41:45 -0500

    resized

--- src/components/FullWidthImage.js
+++ src/components/FullWidthImage.js
@@ -4,7 +4,7 @@ import { GatsbyImage } from "gatsby-plugin-image";
 
 export default function FullWidthImage(props) {
   const {
-    height = auto,
+    height = 450,
     img,
     title,
     subheading,
